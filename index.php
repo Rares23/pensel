4a2f4f1b20cf49e1787348d98605c9750e6edecb
Rares23 <cruceat.rares@yahoo.com>
2014-01-04 16:21:49 +0200

    Test

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title> Pensiunea Elixia</title>
-</head>
-<body>
-	<div>
-		<a href="adm/index.php?id=1">Go to admin</a>
-	</div>
-</body>
+	<head>
+		<title> Pensiunea Elixia</title>
+	</head>
+	<body>
+		<div>
+			<a href="adm/index.php?id=1">Go to admin</a>
+		</div>
+	</body>
 </html>
